2b33e75d152bd9ab01be43a46ba9510476f3af10
Ayo <talktojmcvibes@gmail.com>
2024-03-11 14:22:22 +0100

    filter student by location

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+export default function getStudentsByLocation(listStudents, city){
+    function getCity(student){
+        return student.location == city
+    }
+    const newList = listStudents.filter(getCity)
+    return newList
+}
