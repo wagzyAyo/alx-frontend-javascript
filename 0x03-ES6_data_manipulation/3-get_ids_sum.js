27285a819a6ade53034a1e104965c2172eae47c1
Ayo <talktojmcvibes@gmail.com>
2024-03-11 14:38:09 +0100

    Get ids sum

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,8 @@
+export default function getStudentIdsSum(listOfStudents) {
+    function sum(total, student) {
+        return total + student.id;
+    }
+
+    const number= listOfStudents.reduce(sum, 0);
+    return number;
+}
