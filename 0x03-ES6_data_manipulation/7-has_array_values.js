44ffeecc23367fe6a97d5d4b254110f43497a59d
Ayo <talktojmcvibes@gmail.com>
2024-03-11 21:08:55 +0000

    eslint fix

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,8 @@
-export default function hasValuesFromArray(newSet, array){
-    for (const x of array){
-        if (!newSet.has(x)){
-            return false;
-        };
+export default function hasValuesFromArray(newSet, array) {
+  for (const x of array) {
+    if (!newSet.has(x)) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
