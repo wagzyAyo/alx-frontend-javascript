84279151484b920e72b2ebc1bfb5e60e13c42c24
Ayo <talktojmcvibes@gmail.com>
2024-03-13 13:42:03 +0000

    print teacher function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -5,3 +5,13 @@ interface Teacher {
   yearsOfExperience: number;
   [key: string]: boolean;
 }
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const export printTeacher: printTeacherFunction = (firstName: string, lastname: string): string => `${firstName[0]}. ${lastName}`;
